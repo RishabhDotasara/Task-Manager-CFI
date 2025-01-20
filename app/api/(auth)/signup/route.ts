f46fed50c8c1d9d9fa641f51a997a13fac50b5f8
Rishabh Dotasara <rishabhdotasara@gmail.com>
2025-01-20 18:47:32 +0530

    added speed insights on vercel, reduced app info in about 2

--- app/api/(auth)/signup/route.ts
+++ app/api/(auth)/signup/route.ts
@@ -3,6 +3,8 @@ import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs"
 import { prisma } from "@/lib/prisma";
 
+export const dynamic = "force-dynamic";
+
 export async function POST(request:NextRequest)
 {
     try 
